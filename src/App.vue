d622a6ef592488adb68c0504704025e2326a12ed
Mx. Illusionment <malenchite@gmail.com>
2025-05-29 15:38:37 -0400

    0008: New mechs

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///UNS-RIO-GRANDE-M1//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Irresistible Object"
+          "mech": "Silver Stallion"
         },
         {
           "callsign": "Nexus",
